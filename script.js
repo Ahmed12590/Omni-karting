cda3be414f7adc251d427c5a72d1dd3b796c094e
Ahmed12590 <muhammadahmed12590@gmail.com>
2024-08-12 22:12:46 +0500

    changes

---
+++ script.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.mySwiper', {
+    autoplay: {
+        delay: 3000, // Time in ms between slides
+        disableOnInteraction: false, // Keeps autoplay even when the user interacts with the slider
+    },
+    loop: true, // Loop through slides
+});
